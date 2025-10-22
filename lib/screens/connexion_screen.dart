c268dfe0c19f21d6da52a3f844e238a54a25469a
Mohamed Diop <mohdiopcode@gmail.com>
2025-10-22 20:13:47 +0000

    Adds user connexion logic

--- lib/screens/connexion_screen.dart
+++ lib/screens/connexion_screen.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:m3fund_flutter/constants.dart';
+import 'package:m3fund_flutter/models/requests/authentication_request.dart';
+import 'package:m3fund_flutter/models/responses/exception_response.dart';
+import 'package:m3fund_flutter/screens/customs/custom_text_field.dart';
+import 'package:m3fund_flutter/screens/home_screen.dart';
+import 'package:m3fund_flutter/services/authentication_service.dart';
 import 'package:remixicon/remixicon.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class ConnexionScreen extends StatefulWidget {
   const ConnexionScreen({super.key});
@@ -11,81 +17,230 @@ class ConnexionScreen extends StatefulWidget {
 }
 
 class _ConnexionScreenState extends State<ConnexionScreen> {
-  Future<void> _setIsFirstTimeToFalse() async {
-    final sharedPreferences = await SharedPreferences.getInstance();
-    await sharedPreferences.setBool("isFirstTime", false);
+  final AuthenticationService _authenticationService = AuthenticationService();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _showError = false;
+  String _currentError = "";
+
+  bool isBlankFields() {
+    return (_usernameController.text.trim().isEmpty &&
+        _passwordController.text.trim().isEmpty);
   }
 
   @override
   void initState() {
     super.initState();
-    _setIsFirstTimeToFalse();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _usernameController.dispose();
+    _passwordController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.only(top: 30),
-        child: Center(
-          child: Column(
-            children: [
-              Image.asset("assets/logoName.png", width: 221),
-              SizedBox(height: 15),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.black,
-                  padding: EdgeInsets.zero,
-                  fixedSize: Size(250, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(32),
+      body: Scrollbar(
+        thumbVisibility: false,
+        trackVisibility: false,
+        thickness: 0,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: Center(
+              child: Column(
+                children: [
+                  Image.asset("assets/logoName.png", width: 221),
+                  SizedBox(height: 15),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      padding: EdgeInsets.zero,
+                      fixedSize: Size(250, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32),
+                      ),
+                      elevation: 3,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (_) => const HomeScreen()),
+                      );
+                    },
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Continuer sans se connecter",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 25),
+                          Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: primaryColor,
+                              borderRadius: BorderRadius.circular(32),
+                            ),
+                            child: Icon(
+                              RemixIcons.arrow_right_line,
+                              size: 24,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                        ],
+                      ),
+                    ),
                   ),
-                  elevation: 3,
-                ),
-                onPressed: () {},
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Continuer sans se connecter",
+                  SizedBox(height: 43),
+                  SizedBox(
+                    width: 300,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Connexion",
                         style: TextStyle(
-                          fontSize: 12,
+                          fontSize: 20,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      SizedBox(width: 25),
-                      Container(
-                        width: 40,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          color: primaryColor,
-                          borderRadius: BorderRadius.circular(32),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    hintText: "Téléphone (avec indicatif) ou email",
+                    icon: Icon(
+                      RemixIcons.user_3_line,
+                      size: 24,
+                      color: Color.fromRGBO(0, 0, 0, 0.6),
+                    ),
+                    isPassword: false,
+                    controller: _usernameController,
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    icon: Icon(
+                      RemixIcons.lock_2_line,
+                      size: 24,
+                      color: Color.fromRGBO(0, 0, 0, 0.6),
+                    ),
+                    hintText: "Mot de passe",
+                    isPassword: true,
+                    controller: _passwordController,
+                  ),
+                  SizedBox(height: 8),
+                  if (_showError)
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 280,
+                          child: Text(
+                            _currentError,
+                            style: TextStyle(fontSize: 12, color: Colors.red),
+                          ),
                         ),
-                        child: Icon(
-                          RemixIcons.arrow_right_line,
-                          size: 24,
-                          color: Colors.white,
+                        SizedBox(height: 8),
+                      ],
+                    ),
+                  SizedBox(
+                    width: 280,
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          "Mot de passe oublié?",
+                          style: TextStyle(color: secondaryColor, fontSize: 12),
                         ),
                       ),
-                      SizedBox(width: 5),
-                    ],
+                    ),
                   ),
-                ),
+                  SizedBox(height: 30),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: primaryColor,
+                      foregroundColor: Colors.white,
+                      fixedSize: Size(300, 54),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text("Se connecter", style: TextStyle(fontSize: 24)),
+                    onPressed: () async {
+                      if (isBlankFields()) {
+                        setState(() {
+                          _currentError = "Veuillez remplir tous les champs";
+                          _showError = true;
+                        });
+                      } else {
+                        setState(() {
+                          _showError = false;
+                        });
+                        try {
+                          await _authenticationService.login(
+                            authenticationRequest: AuthenticationRequest(
+                              email: _usernameController.text,
+                              password: _passwordController.text,
+                            ),
+                          );
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (_) => const HomeScreen(),
+                            ),
+                          );
+                        } catch (e) {
+                          setState(() {
+                            ExceptionResponse exception =
+                                ExceptionResponse.fromJson(
+                                  jsonDecode(
+                                    e.toString().replaceAll("Exception: ", ""),
+                                  ),
+                                );
+                            _currentError = exception.message;
+                            _showError = true;
+                          });
+                        }
+                      }
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "ou",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Color.fromRGBO(0, 0, 0, 0.6),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      fixedSize: Size(300, 54),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(color: Color(0xFF2D2D2D), width: 2),
+                      ),
+                    ),
+                    child: Text("S'inscrire", style: TextStyle(fontSize: 24)),
+                    onPressed: () {},
+                  ),
+                  SizedBox(height: 20),
+                ],
               ),
-              SizedBox(height: 43),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Connexion",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
---
+++ lib/screens/customs/custom_text_field.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:remixicon/remixicon.dart';
+
+class CustomTextField extends StatefulWidget {
+  final Icon? icon;
+  final String hintText;
+  final bool isPassword;
+  final TextEditingController controller;
+  const CustomTextField({
+    super.key,
+    required this.icon,
+    required this.hintText,
+    required this.isPassword,
+    required this.controller,
+  });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 62,
+      width: 300,
+      child: TextField(
+        controller: widget.controller,
+        style: const TextStyle(fontSize: 12),
+        obscureText: widget.isPassword ? _obscureText : false,
+        decoration: InputDecoration(
+          hintText: widget.hintText,
+          hintStyle: const TextStyle(color: Colors.grey, fontSize: 12),
+          prefixIconConstraints: BoxConstraints(minHeight: 50, minWidth: 50),
+          prefixIcon: widget.icon != null
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: widget.icon,
+                )
+              : null,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(color: Color(0xFF2D2D2D), width: 2),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(color: Color(0xFF06A664), width: 2),
+          ),
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 23,
+            horizontal: 0,
+          ),
+          suffixIconConstraints: BoxConstraints(minHeight: 50, minWidth: 50),
+          suffixIcon: widget.isPassword
+              ? IconButton(
+                  icon: Icon(
+                    _obscureText
+                        ? RemixIcons.eye_off_line
+                        : RemixIcons.eye_line,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                )
+              : null,
+        ),
+        cursorColor: const Color(0xFF06A664),
+      ),
+    );
+  }
+}
